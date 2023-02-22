40bd2d8ee74c7c8bc9f06277850866661f9d465e
jaeyeoneej <5ikve@naver.com>
2023-02-22 13:34:43 +0900

    edit: #4 테스트 문구로 변경 23-02-22

--- src/Routes/AboutMe.tsx
+++ src/Routes/AboutMe.tsx
@@ -1,35 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  background-color: black;
-`;
-
-const Box = styled.div`
-  background-image: url("http://placekitten.com/800/500");
-  background-repeat: no-repeat;
-  background-size: 110%;
-  background-position: 0 0;
-  /* transition: all 5s; */
-  width: 500px;
-  height: 300px;
-  border: 1px solid red;
-`;
+const Wrapper = styled.div``;
 
 const AboutMe = () => {
-  const boxRef = useRef<HTMLDivElement>(null);
-
-  const onMouseMoveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (boxRef.current) {
-      boxRef.current.style.backgroundPosition =
-        -event.pageX / 10 + "px " + -event.pageY / 10 + "px";
-    }
-  };
-  return (
-    <Wrapper>
-      <Box ref={boxRef} onMouseMove={onMouseMoveHandler} />
-    </Wrapper>
-  );
+  return <Wrapper>{"구현 중입니다. :)"}</Wrapper>;
 };
 
 export default AboutMe;
