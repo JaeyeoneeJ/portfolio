5f6a269bffd8131fc68f17d065c7acc9e0d0e580
jaeyeoneej <5ikve@naver.com>
2023-02-22 14:21:47 +0900

    fixed: #3 브라우저 width 값이 변경됨에 따라 메인이미지의 size 문제 수정 23-02-22

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useRef } from "react";
 import styled from "styled-components";
 import Header from "../Components/Header";
+import useWindowWidth from "../Hooks/useWindowWidth";
 
 const Background = styled.div`
   background-color: white;
@@ -13,7 +14,7 @@ const Wrapper = styled.div`
   background-image: url("https://images.unsplash.com/photo-1595113316349-9fa4eb24f884?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80");
   background-repeat: no-repeat;
   background-position: center 30%;
-  background-size: 103%;
+  background-size: 105%;
   border-radius: 10px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
   max-width: 1600px;
@@ -23,9 +24,9 @@ const Wrapper = styled.div`
   width: 100%;
   overflow: hidden;
   transition: all 2s;
-  /* &:hover {
-    background-position: center 50%;
-  } */
+  @media screen and (max-width: 1400px) {
+    background-size: cover;
+  }
 `;
 const Container = styled.div`
   z-index: 3;
@@ -35,12 +36,19 @@ const Container = styled.div`
 `;
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  let windowWidth = useWindowWidth() > 1400;
   const wrapperRef = useRef<HTMLDivElement>(null);
   const onMouseMoveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (wrapperRef.current) {
+    if (wrapperRef.current && windowWidth) {
       wrapperRef.current.style.transition = "all 0s";
       wrapperRef.current.style.backgroundPosition =
         -event.pageX / 40 + "px " + -event.pageY / 40 + "px";
+    }
+  };
+  const onMouseOverHandler = () => {
+    if (wrapperRef.current && !windowWidth) {
+      console.log("hover");
+      wrapperRef.current.style.backgroundPosition = "center 50%";
     }
   };
   const onMouseLeaveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -55,6 +63,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
         ref={wrapperRef}
         onMouseMove={onMouseMoveHandler}
         onMouseLeave={onMouseLeaveHandler}
+        onMouseOver={onMouseOverHandler}
       >
         <Header />
         <Container>{children}</Container>
