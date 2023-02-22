ae35a71ccc3151e9eb1d60bcdee4eb5074646e69
jaeyeoneej <5ikve@naver.com>
2023-02-22 13:31:41 +0900

    test: #3 마우스 이동에 따른 background position 변경 구현 테스트 23-02-22

--- src/Routes/AboutMe.tsx
+++ src/Routes/AboutMe.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Box = styled.div`
+  background-image: url("http://placekitten.com/800/500");
+  background-repeat: no-repeat;
+  background-size: 110%;
+  background-position: 0 0;
+  /* transition: all 5s; */
+  width: 500px;
+  height: 300px;
+  border: 1px solid red;
+`;
 
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  const onMouseMoveHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (boxRef.current) {
+      boxRef.current.style.backgroundPosition =
+        -event.pageX / 10 + "px " + -event.pageY / 10 + "px";
+    }
+  };
+  return (
+    <Wrapper>
+      <Box ref={boxRef} onMouseMove={onMouseMoveHandler} />
+    </Wrapper>
+  );
 };
 
 export default AboutMe;
